c1077acaa8756a4d733cf11256501dd071e3eef0
curtistinkers <curtistinkers.commit@gmail.com>
2026-08-11 18:21:27 -0500

    Import DebianTemplateCompiler and ProjectManifest in the core entry-point

--- src/package_generator/__init__.py
+++ src/package_generator/__init__.py
@@ -2,6 +2,8 @@
 """Core entry-point exposing public class structures."""
 
 from .builder import DebianPackageBuilder
+from .compiler import DebianTemplateCompiler
 from .logger import Logger
 from .manifest import RepositoryManifest
 from .models import PackageConfig, PackageOSMappingConfig, PackageRepoConfig, ProjectConfig
+from .project_manifest import ProjectManifest
